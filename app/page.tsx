006fdec0d3a7465e97808be00e5f16ddd55d163f
alirafli <mrafli1212@gmail.com>
2026-07-16 14:04:13 +0700

    feat: add many pin point

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,18 @@
 import dynamic from "next/dynamic";
 
 const Map = dynamic(() => import("./Components/Map"), { ssr: false });
+const ManyPinPoint = dynamic(() => import("./Components/ManyPinPoint"), {
+  ssr: false,
+});
 
 export default function Home() {
-  return <Map />;
+  return (
+    <>
+      <h1>First Map Setup</h1>
+      <Map />
+
+      <h1>Many Pin Points</h1>
+      <ManyPinPoint />
+    </>
+  );
 }
